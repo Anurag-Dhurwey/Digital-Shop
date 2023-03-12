605014a61991c3136b83eb7a748989a6df242ae7
Anurag dhurwey <anuragdhurwey9211@gmail.co>
2023-03-12 12:43:34 +0530

    product's Carousel is added in home page

--- src/Components/Mini_components/FilterProducts/Search.js
+++ src/Components/Mini_components/FilterProducts/Search.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useGlobleContext } from '../../../Context/Globle_Context'
 
 const Search = (props) => {
     const {search}=props
+    const {enabled}=useGlobleContext()
   return (
     <>
      <form onSubmit={(e)=>{e.preventDefault()}}>
-        <label htmlFor="search" > Search</label>
-        <input type="name" id='search' name='search' onChange={(e)=>{search(e)}} />
+        <label htmlFor="search" className={`px-2 ${enabled?'text-white':'text-black'}`} > Search</label>
+        <input type="name" id='search' name='search' onChange={(e)=>{search(e)}} className={`${enabled?'bg-zinc-900 text-slate-50 border-l-pink-50 ':'bg-slate-50 text-zinc-900 border-1-black'}`} />
       </form> 
     </>
   )
